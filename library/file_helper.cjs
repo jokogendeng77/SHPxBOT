b283ff9e6074a7be414db43cbbde83119747b4f5
Muchammad Fikri Izzuddin <mbahkoe.pendekar@gmail.com>
2024-02-09 12:36:56 +0700

    update error delay

--- library/file_helper.cjs
+++ library/file_helper.cjs
@@ -1,1 +1,22 @@
-const fs=require("fs").promises,isFileExist=async e=>await fs.access(e).then((()=>!0)).catch((()=>!1))?(console.log("File saved successfully:",e),!0):(console.error("Error: File not saved."),!1),getRandomDelay=(e=3e3,s=3e4)=>Math.floor(Math.random()*(s-e+1)+e);module.exports={getRandomDelay:getRandomDelay,isFileExist:isFileExist};
+const fs = require("fs").promises;
+
+const isFileExist = async (filePath) => {
+    const fileExists = await fs
+        .access(filePath)
+        .then(() => true)
+        .catch(() => false);
+
+      if (fileExists) {
+        console.log("File saved successfully:", filePath);
+        return true;
+      } else {
+        console.error("Error: File not saved.");
+        return false;
+      }
+}
+
+const getRandomDelay = (min=3000, max=30000) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+module.exports = {getRandomDelay, isFileExist}
--- library/streamer.cjs
+++ library/streamer.cjs
@@ -1,1 +1,325 @@
-const ffmpeg=require("fluent-ffmpeg"),util=require("util"),exec=util.promisify(require("child_process").exec),fs=require("fs").promises,{mkdirSync:mkdirSync}=require("fs"),{isFileExist:isFileExist}=require("./file_helper.cjs"),chalk=require("chalk"),path=require("path"),shell=require("shelljs");let ffmpegGlobalProcess=null;function decodeUnicodeEscape(e){return e.replace(/\\u([\d\w]{4})/gi,((e,t)=>String.fromCharCode(parseInt(t,16))))}const videoProcessing=async({liveUrl:e,rtmpServer:t=null,rtmpKey:o=null,isInfiniteMode:r=!1,streamDuration:i=null,baseFilePath:n="",mode:s="stream"})=>{const a=`${n}.flv`,l=`${n}.mp4`;ffmpegGlobalProcess=ffmpeg(e).inputOptions(["-re","-stream_loop -1"]).outputOptions(["-r 30","-s 720x1280","-b:v 800k","-c:v libx264","-preset superfast","-c:a aac","-f flv","-maxrate 800k","-bufsize 800k","-nostats","-hide_banner","-loglevel error"]),t&&o&&(console.log("Re-Steam started!.."),ffmpegGlobalProcess.output(`${t}/${o}`).format("flv")),i&&ffmpegGlobalProcess.duration(i),"restream"===s&&ffmpegGlobalProcess.output(a).format("flv"),ffmpegGlobalProcess.on("error",((e,t,o)=>{console.error(chalk.red("Error:"),e),console.error("ffmpeg stdout:",t),console.error("ffmpeg stderr:",o),cleanup(ffmpegGlobalProcess)})).on("progress",(e=>{})).on("stderr",(e=>console.log(e))).on("end",(async()=>(console.log("Streaming finished."),r&&await handleStreamEnd(r,a,l,t,o),"Streaming finished."))),ffmpegGlobalProcess.run()},reStreamShopee=async({videoUrl:e,rtmpServer:t=null,rtmpKey:o=null,isInfiniteMode:r=!1,streamDuration:i=null})=>{try{let n=null,s=`${__dirname}/../stream_input/${e}`,a="stream";if(!e.includes("mp4")&&!e.includes("flv")){if(n=await getShopeeStreamDetails(e),!n)throw new Error("Stream details could not be retrieved.");s=decodeURIComponent(n.play_url),a="restream"}const l=`${__dirname}/../stream_output/${n?.room_id}-${n?.username}`;await videoProcessing({liveUrl:s,rtmpServer:t,rtmpKey:o,isInfiniteMode:r,streamDuration:i,baseFilePath:l,mode:a})}catch(e){console.error("Error in reStreamShopee function: ",e)}};async function handleStreamEnd(e,t,o,r,i){await convertFlvToMp4(t,o);const n=await isFileExist(o);if(e){console.log(chalk.green("\nRestarting the stream..."));let e=ffmpeg(n?o:t).inputOptions(["-re","-stream_loop -1"]).output(`${r}/${i}`).format("flv").on("error",(t=>{console.error(chalk.red("Error during infinite streaming:"),t),cleanup(e)})).run()}else console.log(chalk.green("\nConversion finished."))}function cleanup(e){e.kill("SIGKILL"),console.log("Cleaning Up ffmpeg..")}const streamDownloader=async({videoUrl:e,durasiVideo:t=null,rtmpServer:o=null,rtmpKey:r=null,isInfinite:i=!1})=>{try{const n=getStreamProvider(e);let s=null;switch(n){case"shopee":case"filestream":s=await reStreamShopee({videoUrl:e,streamDuration:t?60*t:null,rtmpServer:o,rtmpKey:r,isInfiniteMode:i});break;case"tiktok":s=await tiktokDownload({videoUrl:e,duration:t?60*parseInt(t):null,rtmpServer:o,rtmpKey:r,isInfiniteMode:i});break;default:throw Error(`Platform "${n}" Not Supported Yet!`)}return s}catch(e){throw Error(e)}},getStreamProvider=e=>{let t="";if(e.includes("http")){t=new URL(e).hostname}else t="filestream";return t.includes("tiktok.com")?"tiktok":t.includes("youtube.com")?"youtube":t.includes("twitch.tv")?"twitch":t.includes("facebook.com")?"facebook":t.includes("tokopedia.com")?"tokopedia":t.includes("shopee.co.id")?"shopee":t.includes("file")?"filestream":t},getShopeeStreamDetails=async e=>{const t=new URL(e).searchParams.get("session");return fetch(`https://live.shopee.co.id/api/v1/session/${t}`,{headers:{Host:"live.shopee.co.id","Sec-Ch-Ua":'"Brave";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',"Sec-Ch-Ua-Mobile":"?0","User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36","X-Api-Source":"pc","Content-Type":"application/json",Accept:"application/json","X-Shopee-Language":"id","X-Requested-With":"XMLHttpRequest","Sec-Ch-Ua-Platform":'"macOS"',"Sec-Gpc":"1","Accept-Language":"id-ID,id;q=0.6","Sec-Fetch-Site":"same-origin","Sec-Fetch-Mode":"cors","Sec-Fetch-Dest":"empty",Referer:"https://shopee.co.id/?is_from_login=true&is_from_login=true","Accept-Encoding":"gzip, deflate, br"}}).then((async e=>{const t=await e.json();return 0==t.err_code?t?.data?.session:t}))},tiktokStreamData=async e=>{const t=`https://www.tiktok.com/@${e}/live`,o=(await fetch(t,{headers:{"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"}}).then((e=>e.text()))).match(/room_id=(\d+)/);if(!o)throw new Error("No live stream found");const r=o[1];console.info(`\nFound live stream with room id ${r}!`);const i=`https://www.tiktok.com/api/live/detail/?aid=1988&roomID=${r}`,{LiveRoomInfo:n}=await fetch(i).then((e=>e.json())),{title:s,liveUrl:a}=n;return{title:s,liveUrl:a}},tiktokDownload=async({videoUrl:e,output:t="stream_output",format:o="mp4",duration:r=0,rtmpKey:i=null,rtmpServer:n=null,isInfiniteMode:s=!1})=>{try{const a=e.split("/"),l=a[a.length-2].replace("@",""),{title:c,liveUrl:m}=await tiktokStreamData(l),p=t.endsWith(o)?t:`${t.replace(/\/$/,"")}/${l}-${Date.now()}`;mkdirSync(path.dirname(p),{recursive:!0}),await videoProcessing({liveUrl:m,rtmpServer:n,rtmpKey:i,isInfiniteMode:s,streamDuration:r,baseFilePath:p,mode:"restream"}),console.info(`\nDownloading livestream ${c} to /${p}`),console.info("\nCtrl+C to stop downloading and exit")}catch(e){reject(e)}};async function getVideoFileInfo(e){const{stdout:t}=await exec(`ffprobe.exe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 "${e}"`),[o,r]=t.trim().split("x").map(Number);return{width:o,height:r}}async function convertFlvToMp4(e,t){try{const o=await getVideoFileInfo(e),r=`${Math.min(5e3,Math.floor(o.width*o.height/4e3))}k`;await new Promise(((o,i)=>{ffmpeg().input(e).outputOptions(["-movflags frag_keyframe+empty_moov","-c:v libx264","-b:v "+r]).videoCodec("libx264").videoBitrate(r).output(t).on("end",(()=>{console.log("Conversion to MP4 finished."),o()})).on("error",(e=>{console.error("Error during MP4 conversion:",e),i(e)})).run()})),await fs.unlink(e),console.log("FLV file removed:",e)}catch(e){console.error("Error in convertFlvToMp4:",e)}}module.exports={ffmpegGlobalProcess:ffmpegGlobalProcess,streamDownloader:streamDownloader};
+const ffmpeg = require("fluent-ffmpeg");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+const fs = require("fs").promises;
+const {mkdirSync} = require("fs");
+const {isFileExist} = require("./file_helper.cjs");
+const chalk = require("chalk");
+const path = require('path')
+const shell = require('shelljs')
+
+let ffmpegGlobalProcess = null;
+
+function decodeUnicodeEscape(url) {
+  return url.replace(/\\u([\d\w]{4})/gi, (match, grp) => {
+    return String.fromCharCode(parseInt(grp, 16));
+  });
+}
+
+const videoProcessing = async ({liveUrl, rtmpServer = null, rtmpKey = null, isInfiniteMode = false, streamDuration = null, baseFilePath = "", mode = "stream"}) =>{
+  const flvFilePath = `${baseFilePath}.flv`;
+  const mp4FilePath = `${baseFilePath}.mp4`;
+  
+  // Create ffmpeg process
+  ffmpegGlobalProcess = ffmpeg(liveUrl)
+  .inputOptions(["-re", "-stream_loop -1"])
+  .outputOptions([
+    "-r 30",
+    "-s 720x1280",
+    "-b:v 800k",
+    "-c:v libx264",
+    "-preset superfast",
+    "-c:a aac",
+    "-f flv",
+    "-maxrate 800k",
+    "-bufsize 800k", 
+    "-nostats", 
+    "-hide_banner", 
+    "-loglevel error"
+  ]);
+
+if (rtmpServer && rtmpKey) {
+  console.log("Re-Steam started!..");
+  ffmpegGlobalProcess
+    .output(`${rtmpServer}/${rtmpKey}`)
+    .format("flv");
+}
+
+if (streamDuration) {
+  ffmpegGlobalProcess.duration(streamDuration);
+}
+
+if (mode === "restream"){
+  ffmpegGlobalProcess
+  .output(flvFilePath)
+  .format("flv");
+}
+
+// Event listeners for ffmpeg process
+ffmpegGlobalProcess
+  .on("error", (err, stdout, stderr) => {
+    console.error(chalk.red("Error:"), err);
+    console.error("ffmpeg stdout:", stdout);
+    console.error("ffmpeg stderr:", stderr);
+    cleanup(ffmpegGlobalProcess);
+  })
+  .on("progress", (progress) => {
+    // console.log("PROGRESS", progress);
+  })
+  .on("stderr", (stderrLine) => console.log(stderrLine))
+  .on("end", async () => {
+    console.log("Streaming finished.");
+    if(isInfiniteMode){
+      await handleStreamEnd(isInfiniteMode, flvFilePath, mp4FilePath, rtmpServer, rtmpKey);
+    }
+    return "Streaming finished.";
+  });
+
+// Start the ffmpeg process
+ffmpegGlobalProcess.run();
+}
+
+const reStreamShopee = async ({videoUrl, rtmpServer = null, rtmpKey = null, isInfiniteMode = false, streamDuration = null}) => {
+  try {
+    let streamData = null;
+    let liveUrl = `${__dirname}/../stream_input/${videoUrl}`;
+    let mode = "stream";
+    if(!videoUrl.includes("mp4") && !videoUrl.includes("flv")){
+      streamData = await getShopeeStreamDetails(videoUrl);
+      if (!streamData) {
+        throw new Error("Stream details could not be retrieved.");
+      }
+      liveUrl = decodeURIComponent(streamData.play_url);
+      mode = "restream";
+    }
+    const baseFilePath = `${__dirname}/../stream_output/${streamData?.room_id}-${streamData?.username}`;
+
+    await videoProcessing({liveUrl, rtmpServer, rtmpKey, isInfiniteMode, streamDuration, baseFilePath, mode});
+    
+  } catch (error) {
+    console.error("Error in reStreamShopee function: ", error);
+  }
+};
+
+async function handleStreamEnd(isInfiniteMode, flvFilePath, mp4FilePath, rtmpServer, rtmpKey) {
+  // Convert FLV to MP4
+  await convertFlvToMp4(flvFilePath, mp4FilePath);
+  // Check if the file exists
+  const isMp4 = await isFileExist(mp4FilePath);
+  
+  if (isInfiniteMode) {
+    console.log(chalk.green("\nRestarting the stream..."));
+    const videoFile = isMp4 ? mp4FilePath : flvFilePath;
+    // Restart the stream in infinite mode
+    let ffmpegProcess = ffmpeg(videoFile)
+      .inputOptions(["-re", "-stream_loop -1"])
+      .output(`${rtmpServer}/${rtmpKey}`)
+      .format("flv")
+      .on("error", (err) => {
+        console.error(chalk.red("Error during infinite streaming:"), err);
+        cleanup(ffmpegProcess);
+      })
+      .run();
+  } else {
+    console.log(chalk.green("\nConversion finished."));
+  }
+}
+
+function cleanup(ffmpegProcess) {
+  ffmpegProcess.kill('SIGKILL');
+  console.log("Cleaning Up ffmpeg..")
+}
+
+
+const streamDownloader = async ({videoUrl, durasiVideo = null, rtmpServer = null, rtmpKey = null, isInfinite = false}) => {
+  try {
+    const streamProvider = getStreamProvider(videoUrl);
+    let result = null;
+    switch (streamProvider) {
+      case "shopee":
+      case "filestream": 
+        result = await reStreamShopee({videoUrl, streamDuration:durasiVideo?durasiVideo*60:null, rtmpServer, rtmpKey, isInfiniteMode:isInfinite});
+        break;
+  
+      case "tiktok":
+        result = await tiktokDownload({videoUrl:videoUrl, duration:durasiVideo?parseInt(durasiVideo)*60:null, rtmpServer, rtmpKey, isInfiniteMode:isInfinite})
+        break;
+  
+      default:
+        throw Error(`Platform "${streamProvider}" Not Supported Yet!`);
+    }
+    return result;
+  } catch(err) {
+    throw Error(err);
+  }
+
+}
+
+const getStreamProvider = (videoUrl) => {
+  let hostname = "";
+  if(videoUrl.includes("http")){
+    const url = new URL(videoUrl);
+    hostname = url.hostname;
+  }else {
+    hostname = "filestream";
+  }
+  if (hostname.includes("tiktok.com")) {
+    return "tiktok";
+  } else if (hostname.includes("youtube.com")) {
+    return "youtube";
+  } else if (hostname.includes("twitch.tv")) {
+    return "twitch";
+  } else if (hostname.includes("facebook.com")) {
+    return "facebook";
+  } else if (hostname.includes("tokopedia.com")) {
+    return "tokopedia";
+  } else if (hostname.includes("shopee.co.id")) {
+    return "shopee";
+  } else if (hostname.includes("file")) {
+    return "filestream";
+  } else {
+    return hostname;
+  }
+}
+
+const getShopeeStreamDetails = async (videoUrl) => {
+  const url = new URL(videoUrl);
+  const session_id = url.searchParams.get("session");
+  const response = fetch(
+    `https://live.shopee.co.id/api/v1/session/${session_id}`,
+    {
+      headers: {
+        Host: "live.shopee.co.id",
+        "Sec-Ch-Ua":
+          '"Brave";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+        "Sec-Ch-Ua-Mobile": "?0",
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+        "X-Api-Source": "pc",
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "X-Shopee-Language": "id",
+        "X-Requested-With": "XMLHttpRequest",
+        "Sec-Ch-Ua-Platform": '"macOS"',
+        "Sec-Gpc": "1",
+        "Accept-Language": "id-ID,id;q=0.6",
+        "Sec-Fetch-Site": "same-origin",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Dest": "empty",
+        Referer: "https://shopee.co.id/?is_from_login=true&is_from_login=true",
+        "Accept-Encoding": "gzip, deflate, br",
+      },
+    }
+  ).then(async (res) => {
+    const jsonRes = await res.json();
+    if (jsonRes.err_code == 0) {
+      return jsonRes?.data?.session;
+    }
+    return jsonRes;
+  });
+  return response;
+};
+
+const tiktokStreamData = async (username) => {
+  const tiktokUrl = `https://www.tiktok.com/@${username}/live`
+  const textHtml = await fetch(tiktokUrl, {
+    headers: {
+      'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) ' +
+        'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+    },
+  }).then((res) => res.text())
+  const matchRoomId = textHtml.match(/room_id=(\d+)/)
+  if (!matchRoomId) {
+    throw new Error('No live stream found')
+  }
+  const roomId = matchRoomId[1]
+  console.info(`\nFound live stream with room id ${roomId}!`)
+  const api = `https://www.tiktok.com/api/live/detail/?aid=1988&roomID=${roomId}`
+  const {LiveRoomInfo} = await fetch(api).then((res) => res.json())
+  const {title, liveUrl} = LiveRoomInfo;
+
+  return {title, liveUrl};
+}
+
+const tiktokDownload = async ({videoUrl, output="stream_output", format="mp4", duration=0, rtmpKey=null, rtmpServer=null, isInfiniteMode=false}) => {
+  // return new Promise(async (resolve, reject) => {
+    try {
+  // const recDuration = duration > 0 ? `-t ${duration}` : "";
+  const splitUsername = videoUrl.split('/');
+  const tt_username = splitUsername[splitUsername.length - 2].replace('@', '');
+  const {title, liveUrl} = await tiktokStreamData(tt_username);
+  const fileName = output.endsWith(format)
+    ? output
+    : `${output.replace(
+        /\/$/,
+        ''
+      )}/${tt_username}-${Date.now()}`
+  mkdirSync(path.dirname(fileName), { recursive: true })
+  await videoProcessing({liveUrl, rtmpServer, rtmpKey, isInfiniteMode, streamDuration: duration, baseFilePath:fileName, mode:"restream"});
+  // const ffmpegCommand = `ffmpeg -i "${liveUrl}" -r 30 -s 720x1280 -b:v 800k -c:v libx264 -preset superfast -c:a aac -maxrate 800k -bufsize 800k "${fileName}" ${recDuration} -n -nostats -hide_banner -loglevel error`;
+  
+  console.info(`\nDownloading livestream ${title} to /${fileName}`)
+  console.info(`\nCtrl+C to stop downloading and exit`)
+  // Listen for the Ctrl+C signal
+  // shell.exec(ffmpegCommand, { async: true }, (code, stdout, stderr) => {
+  //   if (code === 0) {
+  //     resolve(`Download completed: ${fileName}`);
+  //   } else {
+  //     reject(`Download failed with code ${code}: ${stderr}`);
+  //   }
+  // });
+}catch (error) {
+    reject(error);
+  }
+// });
+}
+
+async function getVideoFileInfo(inputPath) {
+  const { stdout } = await exec(
+    `ffprobe.exe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 "${inputPath}"`
+  );
+  const [width, height] = stdout.trim().split("x").map(Number);
+  return { width, height };
+}
+
+async function convertFlvToMp4(inputPath, outputPath) {
+  try {
+    const videoInfo = await getVideoFileInfo(inputPath);
+    // Determine the best video bitrate based on resolution
+    const maxBitrate = Math.min(
+      5000,
+      Math.floor((videoInfo.width * videoInfo.height) / 4000)
+    );
+    const videoBitrate = `${maxBitrate}k`;
+    await new Promise((resolve, reject) => {
+      ffmpeg()
+        .input(inputPath)
+        .outputOptions([
+          "-movflags frag_keyframe+empty_moov",
+          "-c:v libx264",
+          "-b:v " + videoBitrate,
+        ])
+        .videoCodec("libx264") // Re-encode the video stream
+        .videoBitrate(videoBitrate)
+        .output(outputPath)
+        .on("end", () => {
+          console.log("Conversion to MP4 finished.");
+          resolve();
+        })
+        .on("error", (err) => {
+          console.error("Error during MP4 conversion:", err);
+          reject(err);
+        })
+        .run();
+    });
+
+    // Remove the FLV file
+    await fs.unlink(inputPath);
+    console.log("FLV file removed:", inputPath);
+  } catch (error) {
+    console.error("Error in convertFlvToMp4:", error);
+  }
+}
+
+module.exports = { ffmpegGlobalProcess ,reStreamShopee, getShopeeStreamDetails, convertFlvToMp4, decodeUnicodeEscape, streamDownloader };
